e11f73f689c1818faee027007285dfa097144579
Bernard Cesarz <cesarzb@protonmail.com>
2023-11-06 11:03:10 +0100

    Adding nav links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <main className="App">
-      <Outlet />
-    </main>
+    <div class="layout-elements">
+      <div className="header nav-links">
+        <Link to={`/`} className="brand-link">
+          Main page
+        </Link>
+        <Link to={`/brands`} className="brand-link">
+          Brands list
+        </Link>
+        <Link to={`/cars`} className="brand-link">
+          Cars list
+        </Link>
+      </div>
+      <main className="App">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
